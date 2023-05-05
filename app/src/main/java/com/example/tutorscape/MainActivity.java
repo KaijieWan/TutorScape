668b2feba8863510de28c961d35e9128481ab1a5
Kai <kaijie00@hotmail.com>
2023-05-05 11:31:44 +0800

    Creation of new activities

--- app/src/main/java/com/example/tutorscape/MainActivity.java
+++ app/src/main/java/com/example/tutorscape/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.tutorscape;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView register;
+    private TextView forgetPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = findViewById(R.id.register);
+        forgetPass = findViewById(R.id.forgetPass);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+
+        forgetPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ForgetPassActivity.class));
+                finish();
+            }
+        });
     }
 }
