419ab5ae0b04f06cd76b285b8cf5051db5b770e1
Kai <kaijie00@hotmail.com>
2023-05-08 01:05:12 +0800

    Multiple changes on different activities, important updates on MainActivity

--- app/src/main/java/com/example/tutorscape/MainActivity.java
+++ app/src/main/java/com/example/tutorscape/MainActivity.java
@@ -6,12 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView register;
     private TextView forgetPass;
+    private EditText email;
+    private EditText password;
+    private Button login;
+    
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +30,21 @@ public class MainActivity extends AppCompatActivity {
 
         register = findViewById(R.id.register);
         forgetPass = findViewById(R.id.forgetPass);
-
+        email = findViewById(R.id.editTextTextEmailAddress2);
+        password = findViewById(R.id.editTextTextPassword);
+        login = findViewById(R.id.login);
+        
+        auth = FirebaseAuth.getInstance();
+        
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+                loginUser(txt_email, txt_password);
+            }
+        });
+        
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,6 +57,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, ForgetPassActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, SearchActivity.class));
                 finish();
             }
         });
