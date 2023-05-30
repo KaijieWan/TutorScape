b6999e55cde554c680b0f89a21cba2823af0e281
Kai <kaijie00@hotmail.com>
2023-05-31 00:46:07 +0800

    Working on results page reviews

--- app/src/main/java/com/example/tutorscape/ResultsActivity.java
+++ app/src/main/java/com/example/tutorscape/ResultsActivity.java
@@ -3,6 +3,9 @@ package com.example.tutorscape;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class ResultsActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class ResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        //ProgressBar progressBar1 = findViewById(R.id.progressBar1);
+        //ProgressBar progressBar2 = findViewById(R.id.progressBar2);
+
+        // Update progress for each rating
+        int rating1 = 4;
+        int rating2 = 2;
+
+        int maxRating = 5;  // maximum rating value
+
+        int progress1 = (rating1 * 100) / maxRating;
+        int progress2 = (rating2 * 100) / maxRating;
+
+        //progressBar1.setProgress(progress1);
+        //progressBar2.setProgress(progress2);
+    }
+
+    public void onFavouritesClick(View view){
+        ImageView fav_icon_empty = findViewById(R.id.fav_emptyIcon);
+        fav_icon_empty.setImageResource(R.drawable.icons8_book_and_pencil_96);
+        //Implement logic for adding to user's favourites
     }
 }
