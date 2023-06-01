cfee9556c8bdca661528cb6280421f81d1dafdf2
Kai <kaijie00@hotmail.com>
2023-06-02 01:14:54 +0800

    Review Adapter class done

---
+++ app/src/main/java/com/example/tutorscape/Adapter/ReviewAdapter.java
@@ -0,0 +1,108 @@
+package com.example.tutorscape.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tutorscape.Model.Review;
+import com.example.tutorscape.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class ReviewAdapter extends  RecyclerView.Adapter<ReviewAdapter.ViewHolder>{
+    private Context mContext;
+    private List<Review> mReviews;
+    private String tuitionCentreID;
+
+    public ReviewAdapter(Context mContext, List<Review> mReviews, String tuitionCentreID) {
+        this.mContext = mContext;
+        this.mReviews = mReviews;
+        this.tuitionCentreID = tuitionCentreID;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.results_reviews_items, parent, false);
+        return new ReviewAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Review review = mReviews.get(position);
+
+        if(review.getTCID().equals(tuitionCentreID)){
+            //implement the set text methods here
+            DatabaseReference ref = FirebaseDatabase.getInstance("https://tutorscape-509ea-default-rtdb.asia-southeast1.firebasedatabase.app")
+                    .getReference("Users/" + review.getUID() + "/name");
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String name = snapshot.getValue(String.class);
+                    holder.user_name.setText(name);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+            holder.review_date.setText(review.getReview_date());
+            holder.subjects_enrolled.setText(review.getSubjects_enrolled());
+
+            float rating_float = Float.parseFloat(review.getRating_num());
+            DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+            holder.rating_bar.setRating(Float.parseFloat(decimalFormat.format(rating_float)));
+
+            holder.review_text.setText(review.getReview_text());
+        }
+        else{
+            holder.itemView.setVisibility(View.GONE);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        int count  = 0;
+        for(Review review : mReviews){
+            if(review.getTCID().equals(tuitionCentreID)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView user_name;
+        public TextView review_date;
+        public TextView subjects_enrolled;
+        public RatingBar rating_bar;
+        public TextView review_text;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            user_name = itemView.findViewById(R.id.user_name);
+            review_date = itemView.findViewById(R.id.review_date);
+            subjects_enrolled = itemView.findViewById(R.id.subjects_enrolled);
+            rating_bar = itemView.findViewById(R.id.rating_bar);
+            review_text = itemView.findViewById(R.id.user_review);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tutorscape/Model/Review.java
@@ -0,0 +1,80 @@
+package com.example.tutorscape.Model;
+
+public class Review {
+    private String id;
+    private String UID;
+    private String TCID;
+    private String review_date;
+    private String subjects_enrolled;
+    private String review_text;
+    private String rating_num;
+
+    public Review() {
+    }
+
+    public Review(String id, String UID, String TCID, String review_date, String subjects_enrolled, String review_text, String rating_num) {
+        this.id = id;
+        this.UID = UID;
+        this.TCID = TCID;
+        this.review_date = review_date;
+        this.subjects_enrolled = subjects_enrolled;
+        this.review_text = review_text;
+        this.rating_num = rating_num;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    public String getTCID() {
+        return TCID;
+    }
+
+    public void setTCID(String TCID) {
+        this.TCID = TCID;
+    }
+
+    public String getReview_date() {
+        return review_date;
+    }
+
+    public void setReview_date(String review_date) {
+        this.review_date = review_date;
+    }
+
+    public String getSubjects_enrolled() {
+        return subjects_enrolled;
+    }
+
+    public void setSubjects_enrolled(String subjects_enrolled) {
+        this.subjects_enrolled = subjects_enrolled;
+    }
+
+    public String getReview_text() {
+        return review_text;
+    }
+
+    public void setReview_text(String review_text) {
+        this.review_text = review_text;
+    }
+
+    public String getRating_num() {
+        return rating_num;
+    }
+
+    public void setRating_num(String rating_num) {
+        this.rating_num = rating_num;
+    }
+}
