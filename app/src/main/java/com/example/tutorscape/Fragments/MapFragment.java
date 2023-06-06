461578d0d6951eabf546f5fdc0720db7aa6193f8
Kai <kaijie00@hotmail.com>
2023-06-06 23:33:42 +0800

    Map initializing done

--- app/src/main/java/com/example/tutorscape/Fragments/MapFragment.java
+++ app/src/main/java/com/example/tutorscape/Fragments/MapFragment.java
@@ -2,6 +2,7 @@ package com.example.tutorscape.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -9,9 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.example.tutorscape.R;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapsSdkInitializedCallback {
+    private MapView mapView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,6 +30,22 @@ public class MapFragment extends Fragment {
         if(view == null) {
             Log.e("MapFragment", "Failed to inflate layout");
         }
+        mapView = view.findViewById(R.id.mapView);
+        MapsInitializer.initialize(getContext(), MapsInitializer.Renderer.LATEST, this);
+        mapView.onCreate(savedInstanceState);
+
         return view;
+    }
+
+    @Override
+    public void onMapsSdkInitialized(@NonNull MapsInitializer.Renderer renderer) {
+        //println(it.name)
+        Log.d("TAG", "onMapsSdkInitialized: ");
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                Log.d("OnMapReady", "called");
+            }
+        });
     }
 }
