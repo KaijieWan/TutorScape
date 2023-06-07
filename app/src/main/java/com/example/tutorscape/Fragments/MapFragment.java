7f6107c1bf641c60cf84c71d35fa05277309fbee
Kai <kaijie00@hotmail.com>
2023-06-07 23:47:44 +0800

    More advanced mapping implementation done

--- app/src/main/java/com/example/tutorscape/Fragments/MapFragment.java
+++ app/src/main/java/com/example/tutorscape/Fragments/MapFragment.java
@@ -24,11 +24,17 @@ import com.example.tutorscape.R;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+//import org.geotools.geometry.DirectPosition2D;
+//import org.geotools.referencing.GeodeticCalculator;
 
 import java.io.IOException;
 import java.util.List;
 
 public class MapFragment extends Fragment implements OnMapsSdkInitializedCallback {
+    private static final double EARTH_RADIUS = 6371; // Earth's radius in kilometers
     private MapView mapView;
     private GoogleMap myMap;
     private SearchView mapSearch;
@@ -66,10 +72,57 @@ public class MapFragment extends Fragment implements OnMapsSdkInitializedCallbac
                     }
 
                     Address address = addressList.get(0);
+                    String country = address.getCountryName();
+                    String state = address.getAdminArea();
+                    String city = address.getLocality();
+
                     LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
                     marker = myMap.addMarker(new MarkerOptions().position(latLng).title(location));
-                    myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
 
+                    if(city != null && !city.isEmpty()){
+                        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                    }
+                    else if(state != null && !state.isEmpty()){
+                        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
+                    }
+                    else if(country != null && !country.isEmpty()){
+                        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                    }
+                    else{
+                        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+                    }
+
+                    double [] bounds = calculateBounds(address.getLatitude(), address.getLongitude(), 10);
+                    double minLat = bounds[0];
+                    double maxLat = bounds[1];
+                    double minLon = bounds[2];
+                    double maxLon = bounds[3];
+
+                    // Convert the minimum latitude and longitude to postal code
+                    List<Address> minAddresses = null;
+                    try {
+                        minAddresses = geocoder.getFromLocation(minLat, minLon, 1);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    String minPostalCode = null;
+                    if (minAddresses != null && !minAddresses.isEmpty()) {
+                        minPostalCode = minAddresses.get(0).getPostalCode();
+                    }
+
+                    // Convert the maximum latitude and longitude to postal code
+                    List<Address> maxAddresses = null;
+                    try {
+                        maxAddresses = geocoder.getFromLocation(maxLat, maxLon, 1);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    String maxPostalCode = null;
+                    if (maxAddresses != null && !maxAddresses.isEmpty()) {
+                        maxPostalCode = maxAddresses.get(0).getPostalCode();
+                    }
+                    Query query_levels = FirebaseDatabase.getInstance("https://tutorscape-509ea-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().child("TuitionCentre")
+                            .orderByChild("postal").startAt(minPostalCode).endAt(maxPostalCode);
                 }
                 return false;
             }
@@ -96,5 +149,21 @@ public class MapFragment extends Fragment implements OnMapsSdkInitializedCallbac
                 myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(singapore, 10));
             }
         });
+    }
+
+    public static double[] calculateBounds(double centerLat, double centerLon, double radius) {
+        double latRadian = Math.toRadians(centerLat);
+
+        // Calculate the latitude boundaries
+        double deltaLat = radius / EARTH_RADIUS;
+        double minLat = Math.toDegrees(latRadian - deltaLat);
+        double maxLat = Math.toDegrees(latRadian + deltaLat);
+
+        // Calculate the longitude boundaries
+        double deltaLon = radius / (EARTH_RADIUS * Math.cos(latRadian));
+        double minLon = Math.toDegrees(centerLon - deltaLon);
+        double maxLon = Math.toDegrees(centerLon + deltaLon);
+
+        return new double[] { minLat, maxLat, minLon, maxLon };
     }
 }
