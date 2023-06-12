1061181b64474b2a9a7fcc3e0caf11f64cf5b76e
Kai <kaijie00@hotmail.com>
2023-06-12 23:54:14 +0800

    Started work on filter design

--- app/src/main/java/com/example/tutorscape/Fragments/MapFragment.java
+++ app/src/main/java/com/example/tutorscape/Fragments/MapFragment.java
@@ -19,6 +19,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
