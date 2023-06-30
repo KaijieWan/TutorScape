7e684ca80f14eb1f9734e7328464a2de94557e3e
Kai <kaijie00@hotmail.com>
2023-06-30 23:18:42 +0800

    Working on main settings fragment

---
+++ app/src/main/java/com/example/tutorscape/Fragments/AccountFragment.java
@@ -0,0 +1,23 @@
+package com.example.tutorscape.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.tutorscape.R;
+
+public class AccountFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_settings_account, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/tutorscape/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/tutorscape/Fragments/SettingsFragment.java
@@ -5,15 +5,40 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.transition.ChangeBounds;
 
 import com.example.tutorscape.R;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class SettingsFragment extends Fragment {
+    ChipNavigationBar menu;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    Fragment fragment = null;
+    ImageView expandButton;
+    LinearLayout containerMain;
+
+    ChangeBounds changeBounds = new ChangeBounds();
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.settings_fragment, container, false);
         Log.d("SettingsFragment", "Called");
+
+        menu = root.findViewById(R.id.settings_menu);
+        containerMain = root.findViewById(R.id.container_main);
+        expandButton = root.findViewById(R.id.expand_button);
+
+        if(fragment == null){
+            fragmentManager = getActivity().getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new AccountFragment());
+            fragmentTransaction.commit();
+        }
         return root;
     }
 }
