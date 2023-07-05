732add8e14cc78089433b6720f30f180664c19cd
Kai <kaijie00@hotmail.com>
2023-07-05 16:44:53 +0800

    Security fragment completed, left testing

--- app/src/main/java/com/example/tutorscape/Fragments/SecurityFragment.java
+++ app/src/main/java/com/example/tutorscape/Fragments/SecurityFragment.java
@@ -1,5 +1,6 @@
 package com.example.tutorscape.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -7,12 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
+import com.example.tutorscape.EditReviewActivity;
+import com.example.tutorscape.MainActivity;
 import com.example.tutorscape.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -26,6 +30,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 public class SecurityFragment extends Fragment {
     private EditText currentEmail;
     private EditText currentPassword;
@@ -33,6 +39,7 @@ public class SecurityFragment extends Fragment {
     private EditText confirmPassword;
     private AppCompatButton submitButton;
     private AppCompatButton cancelButton;
+    private AppCompatButton deleteButton;
     private FirebaseAuth firebaseAuth;
     private String curEmail;
     @Nullable
@@ -47,14 +54,15 @@ public class SecurityFragment extends Fragment {
         confirmPassword = view.findViewById(R.id.confirmPassword);
         submitButton = view.findViewById(R.id.submit_button);
         cancelButton = view.findViewById(R.id.cancel_button);
+        deleteButton = view.findViewById(R.id.delete_button);
 
         firebaseAuth = FirebaseAuth.getInstance();
         String userId = firebaseAuth.getUid();
 
-        DatabaseReference ref = FirebaseDatabase.getInstance("https://tutorscape-509ea-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        DatabaseReference emailRef = FirebaseDatabase.getInstance("https://tutorscape-509ea-default-rtdb.asia-southeast1.firebasedatabase.app/")
                 .getReference("Users/" + userId + "/email");
 
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+        emailRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                  curEmail = snapshot.getValue(String.class);
@@ -68,29 +76,151 @@ public class SecurityFragment extends Fragment {
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
-        AuthCredential credential = EmailAuthProvider.getCredential(curEmail, currentPassword.getText().toString());
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //User does not want to update password
-                if(TextUtils.isEmpty(newPassword.getText().toString()) || TextUtils.isEmpty(confirmPassword.getText().toString())){
-                    //Update with whatever is in the email field
-                    user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            //Execute the update to realtime database
-                            //user.updateEmail();
-
+                AuthCredential credential = EmailAuthProvider.getCredential(curEmail, currentPassword.getText().toString());
+                user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //User does not want to change password
+                        if(TextUtils.isEmpty(newPassword.getText().toString()) || TextUtils.isEmpty(confirmPassword.getText().toString())){
+                            updateUserEmail();
                         }
-                    });
-                }
-                else{ //User wants to update password
-                    //Update password along with whatever is in the email field
-                }
+                        else{ //User wants to change password
+                            if(!newPassword.getText().toString().equals(confirmPassword.getText().toString())){
+                                Toast.makeText(getContext(), "Passwords do not match, please enter again!", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                updateUserEmailPass();
+                                Toast.makeText(getContext(), "Email address and password update successful!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentPassword.getText().clear();
+                newPassword.getText().clear();
+                confirmPassword.getText().clear();
+
+                emailRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String fixedEmail = snapshot.getValue(String.class);
+                        currentEmail.setText(fixedEmail);
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Log.d("Security fragment", "Account deleted");
+                            //Update realtime database as well (remove account)
+                            DatabaseReference delRef = FirebaseDatabase.getInstance("https://tutorscape-509ea-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                                    .getReference("Users/" + userId);
+                            delRef.removeValue();
+
+                            Toast.makeText(getContext(), "Account successfully deleted!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getContext(), MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        }
+                    }
+                });
             }
         });
 
         return view;
+    }
+
+    private void updateUserEmailPass() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        user.updateEmail(currentEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Log.d("updateUserEmailPass", "Email update successful");
+                    //Update realtime database as well
+                    String userId = firebaseAuth.getUid();
+                    DatabaseReference emailRef = FirebaseDatabase.getInstance("https://tutorscape-509ea-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                            .getReference("Users/" + userId);
+
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put("email", currentEmail.getText().toString());
+
+                    emailRef.updateChildren(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Log.d("updateUserEmailPass", "update successful");
+                            }
+                            else{
+                                Log.d("updateUserEmailPass", "update failed: " + task.getException().getMessage());
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        user.updatePassword(confirmPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Log.d("updateUserEmailPass", "Password update successful");
+                }
+            }
+        });
+    }
+
+    private void updateUserEmail() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        user.updateEmail(currentEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Log.d("updateUserEmail", "Successful");
+                    Toast.makeText(getContext(), "Email address update successful!", Toast.LENGTH_SHORT).show();
+                    //Update realtime database as well
+                    String userId = firebaseAuth.getUid();
+                    DatabaseReference emailRef = FirebaseDatabase.getInstance("https://tutorscape-509ea-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                            .getReference("Users/" + userId);
+
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put("email", currentEmail.getText().toString());
+
+                    emailRef.updateChildren(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Log.d("updateUserEmailPass", "update successful");
+                            }
+                            else{
+                                Log.d("updateUserEmailPass", "update failed: " + task.getException().getMessage());
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
     }
 }
